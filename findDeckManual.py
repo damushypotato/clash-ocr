cc46d6cc3c0c14e19d7eb867bf0e4853ebced683
damushypotato <70552107+damushypotato@users.noreply.github.com>
2021-11-02 23:55:28 +1100

    hoiyaa

--- findDeckManual.py
+++ findDeckManual.py
@@ -1,9 +1,6 @@
 import numpy as np
 import cv2
-import easyocr
-import pyautogui
 from PIL import Image, ImageTk, ImageFile
-from win32 import win32gui
 from tkinter import Tk, Canvas, constants
 from tkinter import ttk
 import requests
@@ -18,72 +15,8 @@ api_key = os.environ.get("API_KEY")
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-def screenshot(window_title=None) -> Image:
-    if window_title:
-        hwnd = win32gui.FindWindow(None, window_title)
-        if hwnd:
-            win32gui.SetForegroundWindow(hwnd)
-            x, y, x1, y1 = win32gui.GetClientRect(hwnd)
-            x, y = win32gui.ClientToScreen(hwnd, (x, y))
-            x1, y1 = win32gui.ClientToScreen(hwnd, (x1 - x, y1 - y))
-            im = pyautogui.screenshot(region=(x, y, x1, y1))
-            return im
-        else:
-            print('Window not found!')
-    else:
-        im = pyautogui.screenshot()
-        return im
-
-reader = easyocr.Reader(['en'], gpu=False)
-
-def getInfo():
-    ss = screenshot('Bluestacks')
-
-    if not ss:
-        print('Bluestacks not found')
-        exit()
-
-    width, height = ss.size
-    
-    badgeOffset = int(width * 0.08)
-
-    left = badgeOffset
-    top = 33
-    right = (width - 33) * 0.82
-    bottom = height * 0.1
-
-    ss = ss.crop((left, top, right, bottom))
-    img = cv2.cvtColor(np.array(ss), cv2.COLOR_RGB2GRAY)
-
-    levelLabelX = int((width - 33) / 2) - badgeOffset
-
-    cv2.rectangle(img, (levelLabelX - 40, 20), (levelLabelX + 40, 30), (255, 255, 255), -1)
-    cv2.rectangle(img, (levelLabelX, 15), (levelLabelX + 40, 25), (255, 255, 255), -1)
-
-    img = cv2.bitwise_not(img)
-
-    thresh = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,15,2)
-
-    result = reader.readtext(img)
-
-    if len(result) != 2:
-        print('Insufficient player data')
-        cv2.imshow('Data', thresh)
-        cv2.waitKey(0)
-        return
-
-    data = (result[0][1], result[1][1])
-
-    print(f'Player: {data[0]}\nClan: {data[1]}')
-
-    # cv2.imshow('Data', thresh)
-    # cv2.waitKey(0)
-
-    displayDeck(data)
-
-
 def displayDeck(data):
-    clans: list = requests.get(f'https://proxy.royaleapi.dev/v1/clans?name={parse.quote_plus(data[1])}&limit=20', headers={'authorization': f'Bearer {api_key}'}).json()['items']
+    clans: list = requests.get(f'https://proxy.royaleapi.dev/v1/clans?name={parse.quote_plus(data[1])}&limit=50', headers={'authorization': f'Bearer {api_key}'}).json()['items']
     if len(clans) == 0:
         print('Clan not found')
         return
@@ -114,7 +47,7 @@ def displayDeck(data):
     
     results = []
     for m in allMembers:
-        ratio = fuzz.WRatio(data[0], m['name'])
+        ratio = fuzz.ratio(data[0], m['name'])
         results.append(ratio)
         if ratio == 100:
             break
@@ -157,7 +90,7 @@ cardImgs = []
 button = ttk.Button(
     root,
     text='Snipe Deck',
-    command=lambda: getInfo()
+    command=lambda: displayDeck(('Craktor','TEAM ROYAL'))
 )
 
 button.pack(
